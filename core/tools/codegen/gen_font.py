12f23aaa2441a27ac9948778104ad0603178b924
Martin Milata <martin@martinmilata.cz>
2024-04-08 16:52:12 +0200

    fixup! feat(core): add fonts for T3T1

--- core/tools/codegen/gen_font.py
+++ core/tools/codegen/gen_font.py
@@ -440,7 +440,7 @@ if __name__ == "__main__":
     # NOTE: Unifont Bold does not seem to have czech characters
     FaceProcessor("Unifont", "Bold", 16, bpp=1, shaveX=1, ext="otf").write_files()
 
-    FaceProcessor("TTSatoshi", "Medium", 42, ext="otf").write_files()
-    FaceProcessor("TTSatoshi", "Medium", 21, ext="otf").write_files()
-    FaceProcessor("TTSatoshi", "Medium", 18, ext="otf").write_files()
+    FaceProcessor("TTSatoshi", "DemiBold", 42, ext="otf").write_files()
+    FaceProcessor("TTSatoshi", "DemiBold", 21, ext="otf").write_files()
+    FaceProcessor("TTSatoshi", "DemiBold", 18, ext="otf").write_files()
     FaceProcessor("RobotoMono", "Medium", 21).write_files()
